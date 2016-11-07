c742c1638e0ed84cbeac410d0a4c0dc9e9fbadd9
Manish Awale <Manish Awale>
2016-11-07 15:11:58 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Manish on 11/6/2016.
+ */
+public class Main
+{
+        public static void main(String[] args) {
+
+            System.out.println("Hello Intellij IDEA...");
+    }
+}
